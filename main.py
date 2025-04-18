4540cfc015aa8131f73e880aa40b400fc57d7655
Abraham .M <69463303+abraham-ny@users.noreply.github.com>
2025-04-19 00:02:32 +0300

    minor changes

---
+++ main.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    my_pet = Pet("Buddy")
+    my_pet.get_status()
+    my_pet.eat()
+    my_pet.sleep()
+    my_pet.play()
+    my_pet.train("roll over")
+    my_pet.train("fetch")
+    my_pet.show_tricks()
+    my_pet.get_status()
